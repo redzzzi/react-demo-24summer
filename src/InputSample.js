e10d6a1f6af6078d57ddf60c9b26fbd687e36955
redzzzi <myqueen1346@naver.com>
2024-07-02 00:39:24 +0900

    practice : 이름을 입력하는 input 요소에 포커스

--- src/InputSample.js
+++ src/InputSample.js
@@ -5,9 +5,10 @@ function InputSample() {
     name: "",
     nickname: "",
   });
-  const nameInput = useRef(); // useRef 훅을 사용하여 특정 DOM을 선택한다.
+  const nameInput = useRef(); // useRef 훅을 사용하여 특정 DOM을 선택한다. input에 포커스.
 
   const { name, nickname } = inputs;
+
   const onChange = (e) => {
     const { value, name } = e.target;
     setInputs({
@@ -15,6 +16,7 @@ function InputSample() {
       [name]: value, // input 요소 내부의 name과 value 속성
     });
   };
+
   const onReset = () => {
     setInputs({
       name: "",
@@ -29,6 +31,7 @@ function InputSample() {
         name="name"
         placeholder="이름"
         value={name}
+        ref={nameInput}
       ></input>
       <input
         onChange={onChange}
